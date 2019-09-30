b79cb3779c7bd6dcc2a8bed6f1597499d3d320ea
estefaniacr <estefaniac.bts@gmail.com>
2019-09-30 18:46:35 -0500

    agregando login

--- futbol-project/src/app/app.module.ts
+++ futbol-project/src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from './app-routing.module';
 import { CardTeamComponent } from './card-team/card-team.component';
 import { TeamsComponent } from './teams/teams.component';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'teams', 
@@ -16,6 +21,7 @@ const routes: Routes = [
       { path: '', component: TeamsComponent},
     ] 
   },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'teams', pathMatch: 'full'}
 ]
 
@@ -25,11 +31,17 @@ const routes: Routes = [
     CardTeamComponent,
     TeamsComponent,
     MainComponent,
+    NavBarComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),      
+    RouterModule.forRoot(routes), 
+    AppRoutingModule,
+    FontAwesomeModule,   
+    FormsModule,
+    ReactiveFormsModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
